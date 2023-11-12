9bda8935d93c8c48ce1880ffcd7215e642e01e48
MD MONIRUZZAMAN <96796294+Moniruzzaman2525@users.noreply.github.com>
2023-11-12 23:33:43 +0600

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,7 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
-  // frontend_url: process.env.FRONTEND_URL || 'http://localhost:3000',
+  frontend_url: process.env.FRONTEND_URL || 'https://online-services-ui.vercel.app/',
   database_url: process.env.DATABASE_URL,
   default_student_pass: process.env.DEFAULT_STUDENT_PASS,
   default_faculty_pass: process.env.DEFAULT_FACULTY_PASS,
